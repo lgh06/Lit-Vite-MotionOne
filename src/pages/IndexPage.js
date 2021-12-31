31c02cde6ad5db5b48369fefae8cea6fd67b1ee6
Daniel Liu <hnnk@qq.com>
2021-12-31 14:38:35 +0800

    re-order

--- src/pages/IndexPage.js
+++ src/pages/IndexPage.js
@@ -1,8 +1,22 @@
-// Index page logics for executing kinds of JS in modules
-import {animate as testAnimate} from "../Animate.js";
+/**
+ * @param {HTMLElement} element The Element waiting to be animated
+ */
+ export function testAnimate(element) {
+  element.animate([
+    { background: '#ff004d', offset: 0 },
+    { background: '#ff77ab', offset: 0.20 },
+    { background: '#00e756', offset: 0.5 },
+    { background: '#29adff', offset: 0.80 },
+    { background: '#ff77ab', offset: 1 }
+  ], {
+    duration: 2000,
+    direction: 'alternate',
+    iterations: Infinity
+  });
+}
 import {animate} from "motion";
 export default function(){
-  // animate(document.querySelector('first-element'))
+  // testAnimate(document.querySelector('first-element'))
   // animate('first-element', {x: 200},{duration: 5})
   console.log('Index Page')
 }
